2a3dbdbd471c85f9571c3740fb07bc55275c27cb
Nico Born <born.nico@gmail.com>
2022-01-04 16:09:20 +0100

    dashboard

--- src/main/java/ch/fhnw/acrm/controller/DashboardController.java
+++ src/main/java/ch/fhnw/acrm/controller/DashboardController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(path = "/dashboard")
-public class CustomerController {
+public class DashboardController {
 
     @GetMapping
     public String getDashboardView(){
