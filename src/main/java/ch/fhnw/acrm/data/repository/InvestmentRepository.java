e0637a50e18efec439ce1f7ccc754685909c2b0e
Nico Born <born.nico@gmail.com>
2022-01-16 13:32:58 +0100

    edit

---
+++ src/main/java/ch/fhnw/acrm/data/repository/InvestmentRepository.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2020. University of Applied Sciences and Arts Northwestern Switzerland FHNW.
+ * All rights reserved.
+ */
+
+package ch.fhnw.acrm.data.repository;
+
+import ch.fhnw.acrm.data.domain.Expense;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+	Expense findByName(String name);
+	Expense findByDescription(String description);
+	List<Expense> findByAgentId(Long agentId);
+	List<Expense> findByIdAndAgentId(Long expenseId, Long agentId);
+}
