75b3083e84116375bbebe8f39d9817c163ee2c2e
Nico Born <born.nico@gmail.com>
2022-01-17 15:39:22 +0100

    analytics

---
+++ src/main/java/ch/fhnw/acrm/controller/AnalyticsController.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2020. University of Applied Sciences and Arts Northwestern Switzerland FHNW.
+ * All rights reserved.
+ */
+
+package ch.fhnw.acrm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/analytics")
+public class IndexController {
+
+    @GetMapping
+    public String getAnalyticsView(){
+        return "analytics/analytics.html";
+    }
+
+}
