f500be50d0f184b826c186cf12722202735380c9
Nico Born <born.nico@gmail.com>
2022-01-17 15:40:17 +0100

    Update AnalyticsController.java

--- src/main/java/ch/fhnw/acrm/controller/AnalyticsController.java
+++ src/main/java/ch/fhnw/acrm/controller/AnalyticsController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(path = "/analytics")
-public class IndexController {
+public class AnalyticsController {
 
     @GetMapping
     public String getAnalyticsView(){
